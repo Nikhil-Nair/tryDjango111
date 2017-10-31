080bf059af83c494208a8cfbe0ec8602fa83a09d
nikhil-nair <nikhil.nair.2793@gmail.com>
2017-10-31 22:41:26 +0530

    Initial Commit

---
+++ newsletter/views.py
@@ -0,0 +1,57 @@
+from django.conf import settings
+from django.shortcuts import render
+from django.core.mail import send_mail
+
+from .forms import ContactForm, SignUpForm
+# Create your views here.
+def home(request):
+    title = "Sign Up!"
+    form = SignUpForm(request.POST or None)
+
+    context = {
+        "title" : title,
+        "form" : form
+
+    }
+
+    if form.is_valid():
+        instance = form.save(commit = False)
+        if not instance.full_name:
+            instance.full_name = "Justin"
+        instance.save()
+        context = {
+            "title" : "Thank You!",
+
+        }
+
+
+    return render(request, "base.html", context)
+
+def contact(request):
+    form = ContactForm(request.POST or None)
+    if form.is_valid():
+        form_email = form.cleaned_data.get("email")
+        form_message = form.cleaned_data.get("message")
+        form_full_name = form.cleaned_data.get("full_name")
+
+
+        subject = "site Contact Form"
+        from_email = settings.EMAIL_HOST_USER
+        to_email = [from_email,'yourotheremail@email.com']
+        contact_message = "%s: %s via %s"%(
+            form_full_name,
+            form_message,
+            form_email
+        )
+        send_mail(subject,
+         contact_message,
+         from_email,
+         [to_email],
+         fail_silently=False
+        )
+
+
+    context = {
+        "form" : form,
+    }
+    return render(request, "forms.html", context)
